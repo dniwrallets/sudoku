bfa5b616413367d0ef3ca8fc1e5dd3a6bf8d9cb7
John <dniwrallets@gmail.com>
2020-05-07 16:24:12 -0400

    Implemented funcitons to persist new users to database

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +12,12 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
     },
-    (accessToken) => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (!existingUser) {
+          new User({ googleId: profile.id }).save();
+        }
+      });
     }
   )
 );
